6ed6785bf5249ab7ee59c5f5bcbbb671087dc0cd
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 13:27:08 -0500

    Update UnstakeCard.tsx

--- frontend/src/components/UnstakeCard.tsx
+++ frontend/src/components/UnstakeCard.tsx
@@ -1,32 +1,12 @@
-import { useState } from "react"
-import { useStakingWrite } from "../hooks/useStakingWrite"
-import { useStakingRead } from "../hooks/useStakingRead"
-import WorldIdGate from "./WorldIdGate"
+type Props = {
+  onUnstake?: () => void;
+};
 
-export default function UnstakeCard() {
-  const [amount, setAmount] = useState("")
-  const { unstake } = useStakingWrite()
-  const { canUnstake } = useStakingRead()
-
+export function UnstakeCard({ onUnstake }: Props) {
   return (
-    <WorldIdGate>
-      <div>
-        <h3>Unstake</h3>
-
-        <input
-          type="number"
-          placeholder="Amount"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-
-        <button
-          disabled={!canUnstake || !amount}
-          onClick={() => unstake(amount)}
-        >
-          Unstake
-        </button>
-      </div>
-    </WorldIdGate>
-  )
+    <div>
+      <h3>Unstake</h3>
+      <button onClick={onUnstake}>Unstake</button>
+    </div>
+  );
 }
