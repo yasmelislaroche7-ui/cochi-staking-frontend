54affe30187309c2ef9cf3b5b74c6238ca0c34cc
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 09:55:57 -0500

    Refactor WORLDCHAIN and update WORLD_ID constants

--- frontend/src/config/world.ts
+++ frontend/src/config/world.ts
@@ -1,14 +1,11 @@
-// src/config/world.ts
-
-export const WORLD_ID_APP_ID = "app_f84357b08826b22ace9ea93d03aef932";
-export const WORLD_ID_ACTION_ID = "verificacion";
-
-/**
- * Worldchain config
- * Ajusta RPC si usas otro proveedor
- */
 export const WORLDCHAIN = {
-  id: 480,
-  name: "Worldchain",
+  chainId: 480,
+  name: "WorldChain Mainnet",
   rpcUrl: "https://worldchain-mainnet.g.alchemy.com/public",
+};
+
+export const WORLD_ID = {
+  appId: "app_f84357b08826b22ace9ea93d03aef932",
+  actionId: "matrix-staking-verification",
+  addressBook: "0x57b930D551e677CC36e2fA036Ae2fe8FdaE0330D",
 };
