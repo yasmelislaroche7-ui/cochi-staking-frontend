6d95d8be9a950b9147bc5cdbafe4760065b25142
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 10:00:08 -0500

    Add token contract and approval functions

--- frontend/src/services/token.service.ts
+++ frontend/src/services/token.service.ts
@@ -1,1 +1,15 @@
+import { ethers } from "ethers";
+import ERC20 from "../abi/ERC20.json";
+import { CONTRACTS } from "../config/contracts";
 
+export const getTokenContract = (signer: ethers.Signer) =>
+  new ethers.Contract(CONTRACTS.TOKEN, ERC20, signer);
+
+export const approveToken = async (
+  signer: ethers.Signer,
+  amount: bigint
+) => {
+  const token = getTokenContract(signer);
+  const tx = await token.approve(CONTRACTS.STAKING, amount);
+  return tx.wait();
+};
