e1576a4eccf4daad3305c1806b90b9b94ba2275d
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 10:45:13 -0500

    Update AppProvider.tsx

--- frontend/src/provider/AppProvider.tsx
+++ frontend/src/provider/AppProvider.tsx
@@ -1,1 +1,5 @@
+import { WalletProvider } from "./WalletProvider"
 
+export function AppProvider({ children }: { children: React.ReactNode }) {
+  return <WalletProvider>{children}</WalletProvider>
+}
