545685e1321682625e47f0f4117ac3feda7d043b
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 01:30:47 -0500

    Add providers and Dashboard component to App

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,1 +1,17 @@
+import Dashboard from "./pages/Dashboard";
 
+// Providers
+import { WorldWalletProvider } from "./provider/WorldWalletProvider";
+import { WorldIdProvider } from "./provider/WorldIdProvider";
+
+function App() {
+  return (
+    <WorldWalletProvider>
+      <WorldIdProvider>
+        <Dashboard />
+      </WorldIdProvider>
+    </WorldWalletProvider>
+  );
+}
+
+export default App;
