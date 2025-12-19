b811548153926406321a6da73dd5bab671b6fa52
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 01:29:04 -0500

    Wrap App component with WorldProvider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import { WorldProvider } from "./provider/WorldProvider";
 
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WorldProvider>
+      <App />
+    </WorldProvider>
+  </React.StrictMode>
+);
