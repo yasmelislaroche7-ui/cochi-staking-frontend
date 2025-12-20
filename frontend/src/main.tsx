34ea66d2b0fb84ff3f4ef7db8cab8d0858f64720
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 20:34:46 -0500

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { AppProvider } from "./provider/AppProvider";
-
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 ).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
