b33e98c626ccfd4b8958acad0cabd9a96e80906c
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2025-12-19 20:30:20 -0500

    Update constants.ts

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,2 +1,3 @@
+export const REFRESH_INTERVAL = 1000; // 1 segundo (realtime rewards)
 export const YEAR = 365 * 24 * 60 * 60;
-export const UNSTAKE_DELAY = 24 * 60 * 60;
+export const UNSTAKE_DELAY = 7 * 24 * 60 * 60;
